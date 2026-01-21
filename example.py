04bc8b8f817d046e8c971d88d7ecfc688562f6bb
akshaayan <akshaayan@gmail.com>
2026-01-21 13:57:21 +0500

    latest commit

---
+++ example.py
@@ -0,0 +1,18 @@
+# r = "Hello"
+# d = "World"
+
+
+
+# year = 2026 
+# print(type(year))
+# print(r+' '+d + ' ', year) 
+
+# st = "12"
+# st_n = int(st)
+# d = 129
+# d = chr(d)
+# print(d)
+# print(type(d))
+
+nam = input('Who are you? ')
+print('Welcome', nam)
